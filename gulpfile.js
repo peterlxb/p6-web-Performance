9a98c2b30bfdc1fcbfaf0f0b7cef3e78bc5371c9
peterlxb <peterlxb@gmail.com>
2017-06-10 08:48:02 +0800

    add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
+gulp.task('minify', function() {
   //the default
+  return gulp.src('src/views/js/main.js')
+         .pipe(uglify())
+         .pipe(gulp.dest('Test-folder'))
+
 });
