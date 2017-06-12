f7f3eccaf87a6c1252eac3a01db7c95264169ac8
peterlxb <peterlxb@gmail.com>
2017-06-12 17:03:51 +0800

    add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify', function() {
   //the default
-  return gulp.src('src/views/js/main.js')
+  return gulp.src(['src/views/js/*.js','src/js/*.js'])
          .pipe(uglify())
-         .pipe(gulp.dest('Test-folder'))
+         .pipe(gulp.dest('Test-folder/js'))
 
 });
+
+gulp.task('minify-css',function(){
+  return gulp.src('src/css/*.css')
+         .pipe(cleanCSS({compatibility: 'ie8'}))
+         .pipe(gulp.dest('Test-folder/css'))
+})
